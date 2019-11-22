a731cf9f58ed39fb204b8b293451467d421668f5
kayky lopes <kaykylopes@hotmail.com>
2019-11-21 22:23:03 -0300

    Criacao das Entidades

---
+++ Quickbuy.Dominio/Entidades/Entidade.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quickbuy.Dominio.Entidades
+{
+    public abstract class Entidade
+    {
+    }
+}
---
+++ Quickbuy.Dominio/Enumerados/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quickbuy.Dominio.Enumerados
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        naoDefinido = 0,
+        Boleto = 1,
+        CartaoCredito = 2,
+        Deposito =3
+    }
+}
